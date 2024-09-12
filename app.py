6c40a411d65fd5f566ee8a766611dfd84aefe83b
Tobin Elavathil <tobin.elavathil@gmail.com>
2024-09-12 11:02:05 -0400

    add .flake8 .pre-commit-config.yaml

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import os
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from openai import OpenAI
 from pydub import AudioSegment
 
 # Load environment variables
-load_dotenv('.envrc')
+load_dotenv(".envrc")
 
 # initialize openai api
 client = OpenAI()
@@ -13,41 +12,45 @@ client = OpenAI()
 # Initialize Flask app
 app = Flask(__name__)
 
-@app.route('/api/voice-to-text', methods=['POST'])
+
+@app.route("/api/voice-to-text", methods=["POST"])
 def voice_to_text():
-  # Get the audio file from the request
-  audio_file = request.files['file']
+    """Function to convert voice to text"""
+    # Get the audio file from the request
+    audio_file = request.files["file"]
 
-  # Convert the audio file to wav format
-  audio = AudioSegment.from_file(audio_file)
-  audio.export('converted_audio.wav', format='wav')
+    # Convert the audio file to wav format
+    audio = AudioSegment.from_file(audio_file)
+    audio.export("converted_audio.wav", format="wav")
 
-  # Read the audio file
-  with open('converted_audio.wav', 'rb') as file:
-    transcription = client.audio.transcriptions.create(
-      model="whisper-1",
-      file=audio_file,
+    # Read the audio file
+    with open("converted_audio.wav", "rb") as file:
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=file,
+        )
+
+    # Return the transcription
+    return jsonify(transcription)
+
+
+@app.route("/api/text-to-chat", methods=["POST"])
+def text_to_chat():
+    """Function to convert text to chat"""
+    # Get the text form the request
+    data = request.get_json()
+    text = data["text"]
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": text},
+        ],
     )
 
-  # Return the transcription
-  return jsonify(transcription)
+    return jsonify({"response": completion.choices[0].message})
 
-@app.route('/api/text-to-chat', methods=['POST'])
-def text_to_chat():
-  # Get the text form the request
-  data = request.get_json()
-  text = data['text']
 
-  completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "You are a helpful assistant."},
-      {"role": "user", "content": text}
-    ]
-  )
-
-  return jsonify({'response': completion.choices[0].message})
-
-if __name__ == '__main__':
-  app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(debug=True)
